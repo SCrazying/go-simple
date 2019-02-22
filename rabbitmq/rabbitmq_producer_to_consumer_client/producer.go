dcf10180cccd08fe1a454a7124663702e32d8e86
junyou wang <locojyw@outlook.com>
2019-02-22 18:04:20 +0800

    添加消费生产队列

---
+++ rabbitmq/rabbitmq_producer_to_consumer_client/producer.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+func main() {
+
+	//生产者连接到RabbitMQ Broker，建立连接
+	conn, err := amqp.Dial("amqp://admin:admin@192.168.0.16:5672/")
+	failError(err)
+	defer conn.Close()
+
+	//
+	ch, err := conn.Channel()
+	failError(err)
+
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		"hello_exchange",
+		"topic",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failError(err)
+
+	q, err := ch.QueueDeclare(
+		"test",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failError(err)
+	err = ch.QueueBind(q.Name, "test_key", "hello_exchange", false, nil)
+
+	failError(err)
+
+	ch.Publish(
+		"hello_exchange",
+		"test_key",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("test_exchange"),
+			AppId:       "test_yyy",
+		},
+	)
+}
